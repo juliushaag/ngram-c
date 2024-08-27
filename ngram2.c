70646f2a8c5dee665bcfe576001624f8a176e111
haagjulius <haag.julius@outlook.de>
2024-08-27 11:05:44 +0200

    Removed debug code and compacted allocations

--- ngram2.c
+++ ngram2.c
@@ -11,6 +11,8 @@ char** read_names(int* count_out) {
 
   FILE* names = fopen("names.txt", "r");
 
+  if (names == NULL) return NULL;
+
   fseek(names, 0, SEEK_END);
 
   size_t length = ftell(names);
@@ -28,15 +30,14 @@ char** read_names(int* count_out) {
 
   char** names_ptr = malloc(*count_out * sizeof(char*));
 
-  size_t index = 1;
-  for(size_t i = 0; i < length - 1; i++) {
+  size_t index = 0;
+  names_ptr[index++] = names_buffer;
+  for(size_t i = 0; i < length; i++) {
     if (names_buffer[i] == '\n') {
       names_ptr[index++] = names_buffer + i + 1;
       names_buffer[i] = '\0';
     }
   }
-
-  names_ptr[0] = names_buffer;
 
   return names_ptr;
 }
@@ -50,33 +51,6 @@ char itos(int i) {
   if (i == 0) return '.';
   return i + 'a' - 1;
 }
-
-void pbuffer(const float* buffer) {
-  int n = buffer[-1];
-
-  float sum = 0.0f;
-  float min = 0.0f, max = 0.0f;
-
-  for (int i = 0; i < n; i++) {
-    float val = buffer[i];
-    sum += val;
-    if (val > max) max = val;
-    if (val < min) min = val;
-  }
-
-  float mean = sum / (float)n;
-  float std = 0.0f;
-  
-  for (int i = 0; i < n; i++) {
-    std += (buffer[i] - mean) * (buffer[i] - mean);
-  }
-
-  std = std / (float)(n - 1);
-
-
-  printf("mean: %f, std: %f, min: %f, max: %f length: %i\n", mean, sqrt(std), min, max, (int)buffer[-1]);
-}
-
 
 #define N_VOCAB 27
 
@@ -127,20 +101,7 @@ size_t build_data_set(char** names, size_t n_names, int n_context, int **X, int 
   return n_data_set;
 } 
 
-double randn() {
-  while (1) {
-    double u = ((rand() / (double)RAND_MAX) * 2) - 1.0;
-    double v = ((rand() / (double)RAND_MAX) * 2) - 1.0;
 
-    double q = u*u + v*v;
-
-    if (q == 0 || q >= 1) continue;
-
-    double p = sqrt(-2 * log(q) / q);
-
-    return u * p;
-  }
-}
 
 
 void linear_forward(int B, int C, int D, 
@@ -250,7 +211,7 @@ void emb_backward(int B, int C, int I, int T, const int* X, const float* dout, f
   X (I, T)
   dout (I, C * T)
   */ 
-  memset(demb, 0, demb[-1] * sizeof(float));
+  memset(demb, 0, B * C * sizeof(float));
   for (int i = 0; i < I; i++) {
     for (int t = 0; t < T; t++) {
       for (int k = 0; k < C; k++) {
@@ -267,7 +228,6 @@ float cross_entropy(int B, int C, const float* input, const int* Y) {
   Y (B)
   */
   float probssum = 0.0f;
-
   for (int i = 0; i < B; i++) {
     float max = 0.0f;
     for (int j = 0; j < C; j++) {
@@ -310,44 +270,6 @@ void cross_entropy_backward(int B, int C, const float* logits, const int* Y, flo
     dlogits[i] /= (float)B;  
   }
 }
-
-void* buffers[512];
-int buffer_idx = 0;
-
-float *alloc_buffer(int n) {
-  float *buffer = malloc((n + 1) * sizeof(float));
-  buffers[buffer_idx++] = buffer;
-
-  buffer[0] = n;
-  buffer++;
-  return buffer;
-}
-
-float* randn_buffer(int n) {
-  float *buffer = alloc_buffer(n);
-  for (int i = 0; i < n; i++) buffer[i] = randn();
-  return buffer;
-}
-
-float* zeros_buffer(int n) {
-  float *buffer = alloc_buffer(n);
-  memset(buffer, 0, n * sizeof(float));
-  return buffer;
-}
-
-void free_buffers() {
-  for (int i = 0; i < buffer_idx; i++) {
-    free(buffers[i]);
-  }
-}
-
-void mul_buffer(float* buffer, float scalar) {
-  int n = buffer[-1];
-  for (int i = 0; i < n; i++) {
-    buffer[i] *= scalar;
-  }
-}
-
 
 void softmax(int B, int C, float* data) {
   for (int i = 0; i < B; i++) {
@@ -406,51 +328,110 @@ typedef struct ngram_model {
   void* optim_buffers;
 } ngram_model;
 
+
+double randn() {
+  while (1) {
+    double u = ((rand() / (double)RAND_MAX) * 2) - 1.0;
+    double v = ((rand() / (double)RAND_MAX) * 2) - 1.0;
+
+    double q = u*u + v*v;
+
+    if (q == 0 || q >= 1) continue;
+
+    double p = sqrt(-2 * log(q) / q);
+
+    return u * p;
+  }
+}
+
 void model_init(ngram_model* model, int n_embd, int n_hidden, int n_context) {
+
+  float* buffer = calloc(
+    N_VOCAB * n_embd * 2 + 
+    n_context * n_embd * n_hidden * 2 +
+    n_hidden * 2 +
+    n_hidden * N_VOCAB * 2 +
+    N_VOCAB * 2,
+    sizeof(float)
+  );
   
-  model->Emb = randn_buffer(N_VOCAB * n_embd);
+  model->dEmb = buffer;
+  buffer += N_VOCAB * n_embd;
+  model->Emb = buffer;
+  buffer += N_VOCAB * n_embd;
+  for (int i = 0; i < N_VOCAB * n_embd; i++) model->Emb[i] = randn();
 
-  model->W1 = randn_buffer(n_context * n_embd * n_hidden);
-  mul_buffer(model->W1, (5/3) / sqrt(n_context * n_embd));
+  model->dW1 = buffer;
+  buffer += n_context * n_embd * n_hidden;
+  model->W1 = buffer;
+  buffer += n_context * n_embd * n_hidden;
+  for (int i = 0; i < n_context * n_embd * n_hidden; i++) model->W1[i] = randn() * ((5/3) / sqrt(n_context * n_embd));
 
-  model->b1 = randn_buffer(n_hidden);
-  mul_buffer(model->b1, 0.01);
+  model->db1 = buffer;
+  buffer += n_hidden;
+  model->b1 = buffer;
+  buffer += n_hidden;
+  for (int i = 0; i < n_hidden; i++) model->b1[i] = randn() * 0.01f;
 
-  
-  model->W2 = randn_buffer(n_hidden * N_VOCAB);
-  mul_buffer(model->W2, 1 / sqrt(n_hidden));
+  model->dW2 = buffer;
+  buffer += n_hidden * N_VOCAB;
+  model->W2 = buffer;
+  buffer += n_hidden * N_VOCAB;
+  for (int i = 0; i < n_hidden * N_VOCAB; i++) model->W2[i] = randn() / sqrt(n_hidden);
 
-  model->b2 = randn_buffer(N_VOCAB);
-  mul_buffer(model->b2, 0.01);
-  
-  model->dEmb = zeros_buffer(N_VOCAB * n_embd);
-
-  model->dW1 = zeros_buffer(n_context * n_embd * n_hidden);
-  model->db1 = zeros_buffer(n_hidden);
-  
-  model->dW2 = zeros_buffer(n_hidden * N_VOCAB);
-  model->db2 = zeros_buffer(N_VOCAB);
-
+  model->db2 = buffer;
+  buffer += N_VOCAB;
+  model->b2 = buffer; 
+  buffer += N_VOCAB;
+  for (int i = 0; i < N_VOCAB; i++) model->b2[i] = randn() * 0.01;
   
   model->n_embd = n_embd;
   model->n_hidden = n_hidden;
   model->n_context = n_context;  
+  model->n_batch = 0;
 }
 
 void model_prepare_run(ngram_model* model, int batch_size) {
   
-  model->embcat = zeros_buffer(batch_size * model->n_context * model->n_embd);
-  model->hpreact = zeros_buffer(batch_size * model->n_hidden);
-  model->h = zeros_buffer(batch_size * model->n_hidden);
-  model->logits = zeros_buffer(batch_size * N_VOCAB);
-
+  if (model->n_batch) free(model->embcat);
   
-  model->dlogits = zeros_buffer(batch_size * N_VOCAB);  
-  model->dh = zeros_buffer(batch_size * model->n_hidden);
-  model->dhpreact = zeros_buffer(batch_size * model->n_hidden);
-  model->dembcat = zeros_buffer(batch_size * model->n_context * model->n_embd);
 
   model->n_batch = batch_size;
+  if (batch_size == 0) return;
+
+  float *buffer = calloc(
+    batch_size * model->n_context * model->n_embd * 2 + 
+    batch_size * model->n_hidden * 2 + 
+    batch_size * model->n_hidden * 2 + 
+    batch_size * N_VOCAB * 2,
+    sizeof(float)
+  );
+
+  model->embcat =  buffer;
+  buffer += batch_size * model->n_context * model->n_embd;
+  model->dembcat = buffer;
+  buffer += batch_size * model->n_context * model->n_embd;
+
+  model->hpreact =  buffer;
+  buffer += batch_size * model->n_hidden;
+  model->dhpreact = buffer;
+  buffer += batch_size * model->n_hidden;
+  
+  model->h = buffer; 
+  buffer += batch_size * model->n_hidden;
+  model->dh = buffer;
+  buffer += batch_size * model->n_hidden;
+  
+  model->logits = buffer;
+  buffer += batch_size * N_VOCAB;
+  model->dlogits = buffer;
+  buffer += batch_size * N_VOCAB;  
+}
+
+void model_free(ngram_model* model) {
+
+  free(model->dEmb);
+  model_prepare_run(model, 0); 
 }
 
 float* forward(ngram_model* model, int* X) {
@@ -477,7 +458,6 @@ void backward(ngram_model* model, int* X, int* Y) {
   linear_backward(model->n_batch, model->n_embd * model->n_context, model->n_hidden, model->embcat, model->dhpreact, model->W1, model->dW1, model->db1, model->dembcat);
 
   emb_backward(N_VOCAB, model->n_embd, model->n_batch, model->n_context, X, model->dembcat, model->dEmb);
-
 }
 
 void optimize(ngram_model* model, float lr) {
@@ -502,12 +482,17 @@ void optimize(ngram_model* model, float lr) {
 
 int main() {
 
-  srand(42);   // Set random seed.
-
+  srand(42);
 
   // Read names in 
   int names_count = 0;
   char** names = read_names(&names_count);
+
+  if (names == NULL) {
+    printf("Failed to read names.txt\n");
+    return 0;
+  }
+
 
   char* names_buffer = names[0];
 
@@ -529,9 +514,8 @@ int main() {
   model_init(&model, n_embd, n_hidden, n_context);
 
   /* Processing data */
-
   float train_split = 0.8;
-  float test_split = 0.1;
+  float test_split = 0.2;
 
   int* Xtr, *Ytr = NULL;
   size_t n_train = build_data_set(names, (int)(names_count * train_split), n_context, &Xtr, &Ytr); // 80%
@@ -539,31 +523,24 @@ int main() {
   int* Xte, *Yte = NULL;
   size_t n_test = build_data_set(names + (int)(names_count * train_split), (int)names_count * test_split, n_context, &Xte, &Yte); // 10%
 
-  int* Xdev, *Ydev = NULL;
-  size_t n_dev = build_data_set(names + (int)(names_count * (train_split + test_split)), (int)names_count *  (1 - test_split - train_split), n_context, &Xdev, &Ydev); // 10%
+  printf("== Split: train %li, test %li\n", n_train, n_test);
 
-  printf("== Split: train %li, test %li, dev %li\n", n_train, n_test, n_dev);
-
-  clock_t clock_start, clock_end;
-  clock_start = clock();
+  clock_t clock_start = clock();
 
 
   /* Training */
-  
-  
   int bs = 32;
   float lr = 0.1;
   int steps = 10000;
+  printf("== Training for %i steps\n", steps);
 
   model_prepare_run(&model, bs);
 
-  int *X = malloc(sizeof(int) * bs * n_context);
-  int *Y = malloc(sizeof(int) * bs * 1        );
+  int *X = calloc(bs * n_context, sizeof(int));
+  int *Y = calloc(bs * 1        , sizeof(int));
 
 
   float running_loss = 0.0f;
-
-  printf("== Training for %i steps\n", steps);
   for (int step = 0; step < steps; step++) {
 
     for (int i = 0; i < bs; i++) {
@@ -581,7 +558,6 @@ int main() {
     running_loss += loss;
 
     backward(&model, X, Y);
-
     optimize(&model, lr);
 
     if (step % 1000 == 0) {
@@ -589,9 +565,15 @@ int main() {
       running_loss = 0.0f;
     }
     if (step == 10000) lr = 0.01; // lr decay
-  }
+  }  
+  
+  free(X);
+  free(Y);   
 
-  clock_end = clock();
+  free(Xtr);
+  free(Ytr);
+
+  clock_t clock_end = clock();
   double cpu_time_used = ((double) (clock_end - clock_start)) / CLOCKS_PER_SEC;
 
   printf("== Time used %.2fs, step/s: %.2f\n", cpu_time_used, (float)steps / cpu_time_used);
@@ -602,11 +584,12 @@ int main() {
 
   float* logits = forward(&model, Xte);
   float loss = cross_entropy(n_test, N_VOCAB, logits, Yte);  
-
   printf("== Test loss: %f\n", loss);
+
+  free(Xte);
+  free(Yte);
   
   // Generate examples
-
   int n_gen = 10;
   printf("== Generating %i examples\n", n_gen);
 
@@ -633,25 +616,13 @@ int main() {
     }
     puts("");
   }
-
   free(context);
 
-  free_buffers();
-  free(X);
-  free(Y);
 
-  free(Xtr);
-  free(Ytr);
 
-  free(Xte);
-  free(Yte);
-
-  free(Xdev); 
-  free(Ydev);
-
+  model_free(&model);
   free(names_buffer);
   free(names);
-
   return 0;
 }
 
