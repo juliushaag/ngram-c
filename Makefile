346b286cdade66adf31ec34e9c85ddb83f9078de
haagjulius <haag.julius@outlook.de>
2024-08-27 11:23:08 +0200

    cleaned up files

--- Makefile
+++ Makefile
@@ -1,14 +1,7 @@
 
-test: 
-	gcc -shared -D TEST -o tests/ngram2.so -fpic ngram2.c
-	python tests/test_linear.py
-
-default: ngram2
-
-ngram2: ngram2.c
-	gcc ngram2.c -lm -o ngram -g
-
 ngram: ngram.c
 	gcc ngram.c -lm -o ngram
 
-.PHONY: test
+test: ngram.c
+	gcc -shared -D TEST -o tests/ngram2.so -fpic ngram.c
+	python tests/test_linear.py
--- tests/test_linear.py
+++ tests/test_linear.py
@@ -28,9 +28,7 @@ out = out.view(out.shape[0], -1)
 
 out_buffer = torch.zeros_like(out)
 
-emb_forward = ngram["emb_forward"]
-
-emb_forward(n_vocab, n_emb, n_batch, n_context, tc(EMB), tc(X), tc(out_buffer))
+ngram.emb_forward(n_vocab, n_emb, n_batch, n_context, tc(EMB), tc(X), tc(out_buffer))
 
 assert torch.allclose(out, out_buffer), out_buffer.mean()
 
@@ -41,18 +39,15 @@ b1 = torch.randn((n_hidden), requires_grad=True)
 
 hpreact = out @ W1 + b1
 
-linear_forward = ngram["linear_forward"]
-
 hpreact_buffer = torch.zeros_like(hpreact)
-linear_forward(n_batch, n_context * n_emb, n_hidden, tc(out), tc(W1), tc(b1), tc(hpreact_buffer))
+ngram.linear_forward(n_batch, n_context * n_emb, n_hidden, tc(out), tc(W1), tc(b1), tc(hpreact_buffer))
 
 assert torch.allclose(hpreact, hpreact_buffer, atol=1e-5)
 
 h = torch.tanh(hpreact)
 hout = torch.zeros_like(h)
 
-tanh_forward = ngram["tanh_forward"]
-tanh_forward(h.shape[0], h.shape[1], tc(hpreact), tc(hout))
+ngram.tanh_forward(h.shape[0], h.shape[1], tc(hpreact), tc(hout))
 
 assert torch.allclose(h, hout)
 
@@ -62,16 +57,15 @@ b2 = torch.randn((n_vocab), requires_grad=True)
 logits = h @ W2 + b2
 logits_buffer = torch.zeros_like(logits)
 
-linear_forward(n_batch, n_hidden, n_vocab, tc(h), tc(W2), tc(b2), tc(logits_buffer))
+ngram.linear_forward(n_batch, n_hidden, n_vocab, tc(h), tc(W2), tc(b2), tc(logits_buffer))
 assert torch.allclose(logits, logits_buffer, atol=1e-5)
 
 
 loss = F.cross_entropy(logits, Y.to(dtype=torch.long))
 
-cross_entropy = ngram["cross_entropy"]
-cross_entropy.restype = ctypes.c_float
+ngram.cross_entropy.restype = ctypes.c_float
 
-assert torch.allclose(torch.tensor(cross_entropy(n_batch, n_vocab, tc(logits), tc(Y))),  loss), "cross_entriopy forward pass failed"
+assert torch.allclose(torch.tensor(ngram.cross_entropy(n_batch, n_vocab, tc(logits), tc(Y))),  loss), "cross_entriopy forward pass failed"
 
 loss.backward()
 
@@ -82,8 +76,7 @@ logit_grad = F.softmax(logits, 1)
 logit_grad[range(n_batch), Y] -= 1
 logit_grad /= n_batch
 
-cross_entropy_backward = ngram["cross_entropy_backward"]
-cross_entropy_backward(n_batch, n_vocab, tc(logits), tc(Y), tc(dlogits))
+ngram.cross_entropy_backward(n_batch, n_vocab, tc(logits), tc(Y), tc(dlogits))
 
 assert torch.allclose(dlogits, logit_grad)
 
@@ -92,8 +85,7 @@ dW2 = torch.zeros_like(W2)
 db2 = torch.zeros_like(b2)
 dh = torch.zeros_like(h)
 
-linear_backward = ngram["linear_backward"]
-linear_backward(n_batch, n_hidden, n_vocab, tc(h), tc(dlogits), tc(W2), tc(dW2), tc(db2), tc(dh))
+ngram.linear_backward(n_batch, n_hidden, n_vocab, tc(h), tc(dlogits), tc(W2), tc(dW2), tc(db2), tc(dh))
 
 assert torch.allclose(db2, b2.grad)
 assert torch.allclose(dW2, W2.grad)
